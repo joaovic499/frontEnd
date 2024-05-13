4852537c7564251050278c9ddcd1a8eedd0348db
joaovic499 <joaovictorsilva60@live.com>
2024-05-13 14:44:11 -0300

    Adicionando a integraçao do ultimo funcionario cadastrado para a pagina

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,16 @@
+import { Funcionario } from './../../funcionarios/funcionario';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   countFuncionario: number;
   spinnerValue: number;
+  ultimoFuncionario: Funcionario;
 
   constructor(private http: HttpClient) { }
 
@@ -21,8 +23,22 @@ export class HomeComponent implements OnInit {
         (error) => {
           console.error("Erro em contar os funcionario", error);
         }
+      );
+
+      this.carregarUltimoFuncionario();
+    }
+
+    carregarUltimoFuncionario(){
+
+      this.http.get<any>('http://localhost:3001/funcionario/ultimo').subscribe(
+        (data) => {
+          this.ultimoFuncionario = data;
+        },
+        (error) => {
+          console.error("Erro em pegar o ultimo funcionario", error);
+        }
       )
-  }
+    }
 
 
 }
